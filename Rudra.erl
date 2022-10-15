757a7d876bff2339b90daf04d598b38d9ad547fa
Mohnish4533 <97322605+Mohnish4533@users.noreply.github.com>
2022-10-15 19:08:09 +0530

    Update Rudra.erl

--- Rudra.erl
+++ Rudra.erl
@@ -2,5 +2,6 @@
 -export([start/0]).
 
 start() ->
-    %{ok, N} = io:read("Enter number of decimal places of sequence to display: "),
-    io:fwrite("~f~n",[22/7]).
+    {ok, N} = io:read("Enter number of decimal places of sequence to display: "),
+    io:fwrite("~f~n", [22/7]),
+    io:fwrite("~w", [floor(((22/7)-3)*(N*10))]).
